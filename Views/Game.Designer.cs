492e4def9575a6574d6bcb4ef975a687f47188a8
Rama <s1206152@student.windesheim.nl>
2025-04-04 08:29:40 +0200

    v

--- Views/Game.Designer.cs
+++ Views/Game.Designer.cs
@@ -86,10 +86,10 @@
             this.button1.AutoSize = true;
             this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.Font = new System.Drawing.Font("Calisto MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(831, 0);
+            this.button1.Location = new System.Drawing.Point(862, 0);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 55);
+            this.button1.Size = new System.Drawing.Size(99, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "Uitloggen";
             this.button1.UseVisualStyleBackColor = false;
@@ -101,9 +101,9 @@
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Location = new System.Drawing.Point(624, 288);
+            this.panel1.Location = new System.Drawing.Point(895, 222);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(328, 103);
+            this.panel1.Size = new System.Drawing.Size(73, 249);
             this.panel1.TabIndex = 1;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -111,7 +111,7 @@
             // 
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.Location = new System.Drawing.Point(209, 16);
+            this.button2.Location = new System.Drawing.Point(3, 0);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 76);
             this.button2.TabIndex = 0;
@@ -121,7 +121,7 @@
             // 
             this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button4.Location = new System.Drawing.Point(132, 16);
+            this.button4.Location = new System.Drawing.Point(6, 163);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(62, 76);
             this.button4.TabIndex = 2;
@@ -129,13 +129,13 @@
             // 
             // button5
             // 
-            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button5.AutoSize = true;
             this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
             this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button5.Location = new System.Drawing.Point(756, 523);
+            this.button5.Location = new System.Drawing.Point(886, 83);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(92, 84);
+            this.button5.Size = new System.Drawing.Size(84, 84);
             this.button5.TabIndex = 3;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
@@ -154,7 +154,7 @@
             // button3
             // 
             this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.Location = new System.Drawing.Point(45, 17);
+            this.button3.Location = new System.Drawing.Point(3, 83);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(65, 74);
             this.button3.TabIndex = 3;
@@ -221,9 +221,9 @@
             this.button6.AutoSize = true;
             this.button6.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.button6.Font = new System.Drawing.Font("Calisto MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(696, 0);
+            this.button6.Location = new System.Drawing.Point(754, 0);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(128, 55);
+            this.button6.Size = new System.Drawing.Size(101, 55);
             this.button6.TabIndex = 7;
             this.button6.Text = "Home";
             this.button6.UseVisualStyleBackColor = false;
@@ -232,7 +232,7 @@
             // 
             this.panel4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.panel4.AutoSize = true;
-            this.panel4.BackColor = System.Drawing.Color.LightCyan;
+            this.panel4.BackColor = System.Drawing.Color.SkyBlue;
             this.panel4.Controls.Add(this.label1);
             this.panel4.Controls.Add(this.label15);
             this.panel4.Controls.Add(this.label14);
@@ -248,7 +248,7 @@
             this.panel4.Controls.Add(this.label4);
             this.panel4.Controls.Add(this.label3);
             this.panel4.Controls.Add(this.label2);
-            this.panel4.Location = new System.Drawing.Point(152, 78);
+            this.panel4.Location = new System.Drawing.Point(152, 74);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(450, 93);
             this.panel4.TabIndex = 7;
@@ -256,7 +256,7 @@
             // panel3
             // 
             this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.panel3.BackColor = System.Drawing.Color.LightCyan;
+            this.panel3.BackColor = System.Drawing.Color.SkyBlue;
             this.panel3.Controls.Add(this.label23);
             this.panel3.Controls.Add(this.label24);
             this.panel3.Controls.Add(this.label26);
